dd586906065c59915aa574c80b9299f0ec122c67
Bartek Popcorn <bartekpopcorn@barteks-mbp.home>
2018-07-25 12:13:24 +0100

    Added .travis.yml file

---
+++ ScrimmageFinder App/helperFiles/AlertController.swift
@@ -0,0 +1,9 @@
+//
+//  AlertController.swift
+//  ScrimmageFinder App
+//
+//  Created by Bartek Popcorn on 18/07/2018.
+//  Copyright © 2018 The App Experts. All rights reserved.
+//
+
+import Foundation
