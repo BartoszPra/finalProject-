185dc8b1a043a119c1d69864413564b56762e726
Bartosz Prazmo <prazmob@gmail.com>
2019-08-05 09:04:21 +0100

    my Scrimmages added

---
+++ ScrimmageFinder App/Main Controllers/MyScrimmages/Cell/MyScrimmagesCell.swift
@@ -0,0 +1,26 @@
+//
+//  MyScrimmagesCell.swift
+//  ScrimmageFinder App
+//
+//  Created by Bartosz Prazmo on 05/08/2019.
+//  Copyright © 2019 The App Experts. All rights reserved.
+//
+
+import UIKit
+
+class MyScrimmagesCell: UITableViewCell {
+
+    @IBOutlet weak var cellLBL: UILabel!
+        
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- ScrimmageFinder App/Main Controllers/MyScrimmages/MyScrimmagesViewController.swift
+++ ScrimmageFinder App/Main Controllers/MyScrimmages/MyScrimmagesViewController.swift
@@ -12,20 +12,48 @@ import FirebaseAuth
 import FBSDKLoginKit
 import GoogleSignIn
 
-class MyScrimmagesViewController: UIViewController {
+class MyScrimmagesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var coordinator: MyScrimmagesCoordinator?
     var scrimmages = [Scrimmage]()
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        self.title = "My Scrimmages"
+        let nib = UINib(nibName: "MyScrimmagesCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "MyScrimmagesCell")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let userID = Auth.auth().currentUser?.uid
         FIRFirestoreService.shared.readWhere(from: .scrimmages,
                                              whereFld: "createdById",
-                                             equalsTo: "juPk8cPh6bc27EdwmL87ThukiSJ2",
+                                             equalsTo: userID!,
                                              returning: Scrimmage.self) { (scrimmages) in
-            self.scrimmages = scrimmages
-            print(self.scrimmages.count)
+                                             self.scrimmages = scrimmages
+                                             self.tableView.reloadData()
+                                            
         }
-
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyScrimmagesCell", for: indexPath) as? MyScrimmagesCell else {return MyScrimmagesCell()}
+        if !self.scrimmages.isEmpty {
+            let scrimmage = scrimmages[indexPath.row]
+            cell.cellLBL.text = scrimmage.name
+            return cell
+        } else {
+            cell.cellLBL.text = ""
+            return cell
+        }
+    }
 }
